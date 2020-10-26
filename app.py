0ef084f67d2ef17c2b4eef288644c6cbd6c3470f
zac <zac-zhang@outlook.com>
2020-10-26 15:48:54 +0800

    add index page

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 #coding:utf-8
-from flask import Flask, url_for
+from flask import Flask, url_for,render_template
 
 app = Flask(__name__)
 
+name = 'Zac Zhang'
+movies = [
+    {'title':'My Neighbor Toronto', 'year':'1998'},
+    {'title':	'Dead	Poets	Society',	'year':	'1989'},
+    {'title':	'A	Perfect	World',	'year':	'1993'},
+    {'title':	'Leon',	'year':	'1994'},
+    {'title':	'Mahjong',	'year':	'1996'},
+    {'title':	'Swallowtail	Butterfly',	'year':	'1996'},
+    {'title':	'King	of	Comedy',	'year':	'1999'},
+    {'title':	'Devils	on	the	Doorstep',	'year':	'1999'},
+    {'title':	'WALL-E',	'year':	'2008'},
+    {'title':	'The	Pork	of	Music',	'year':	'2012'},
+]
+
 @app.route('/')
-def hello():
-    return "hello"
+def index():
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -17,3 +31,4 @@ def test_url_for():
     print(url_for('user_page',name='zac'))
     print(url_for('test_url_for',num='2'))
     return 'Test_page'
+
